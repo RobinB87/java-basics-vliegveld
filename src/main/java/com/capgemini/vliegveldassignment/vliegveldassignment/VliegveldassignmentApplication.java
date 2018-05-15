4ba7128901af2de4522a3c5cf180bfd3ac08c6b0
RobinB87 <38104646+RobinB87@users.noreply.github.com>
2018-05-15 09:35:36 +0200

    Base

---
+++ src/main/java/com/capgemini/vliegveldassignment/vliegveldassignment/VliegveldassignmentApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.vliegveldassignment.vliegveldassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VliegveldassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VliegveldassignmentApplication.class, args);
+	}
+}
